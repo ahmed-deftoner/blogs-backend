ae05637a5a0250aa20188b906654955e37fa725b
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-15 20:58:42 +0500

    extract token

--- api/auth/token.go
+++ api/auth/token.go
@@ -1,7 +1,9 @@
 package auth
 
 import (
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -14,4 +16,17 @@ func CreateToken(user_id uint32) (string, error) {
 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(os.Getenv("API_SECRET")))
+}
+
+func ExtractToken(r *http.Request) string {
+	key := r.URL.Query()
+	token := key.Get("token")
+	if token != "" {
+		return token
+	}
+	bearertoken := r.Header.Get("Authorization")
+	if len(strings.Split(bearertoken, " ")) == 2 {
+		return strings.Split(bearertoken, " ")[1]
+	}
+	return ""
 }
