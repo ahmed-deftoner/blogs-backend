8ecdb4b4596fbcb95733169b760580d23b1c9542
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-16 13:03:54 +0500

    initialize

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
+
+	"github.com/ahmed-deftoner/blogs-backend/api/models"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 )
@@ -8,4 +12,24 @@ import (
 type Server struct {
 	DB     *gorm.DB
 	Router *mux.Router
+}
+
+func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, DbName string) {
+
+	var err error
+
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+	server.DB, err = gorm.Open(Dbdriver, DBURL)
+	if err != nil {
+		fmt.Printf("Cannot connect to %s database", Dbdriver)
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the %s database", Dbdriver)
+	}
+
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{}) //database migration
+
+	server.Router = mux.NewRouter()
+
+	server.initializeRoutes()
 }
