a01c23478401a8b745fdf5b351b55e9a54dcf658
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-16 12:55:48 +0500

    auth middlewae

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -1,10 +1,27 @@
 package middleware
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/ahmed-deftoner/blogs-backend/api/auth"
+	"github.com/ahmed-deftoner/blogs-backend/api/response"
+)
 
 func MiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		r.Header.Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+func MiddlewareAuth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.TokenValid(r)
+		if err != nil {
+			response.ERROR(w, http.StatusUnauthorized, errors.New("unauthorized"))
+			return
+		}
 		next(w, r)
 	}
 }
